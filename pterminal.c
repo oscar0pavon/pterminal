b2141961f0f5029291f4c939bffd1ba99571763d
Oscar Pavon <pavon_oscar@outlook.com>
2026-03-27 06:13:11 +0000

    handle fds with pway

--- pterminal.c
+++ pterminal.c
@@ -14,8 +14,6 @@
 
 static char *shell = "/bin/sh";
 
-struct pollfd pterminal_fds[3];
-
 int set_terminal_cursor(int cursor) {
   if (!BETWEEN(cursor, 0, 7)) /* 7: st extension */
     return 1;
@@ -32,37 +30,21 @@ void *run_pterminal(void *none) {
 
   tty_fd = ttynew(NULL, shell, NULL, NULL);
 
-  struct pollfd tty_poll = {tty_fd, POLLIN, 0};
-  struct pollfd wayland_poll = {pway->fd, POLLIN, 0};
-  struct pollfd keyboard_timer_poll= {pway->keys_timer_fd, POLLIN, 0};
-
-  pterminal_fds[0] = tty_poll;
-  pterminal_fds[1] = wayland_poll;
-  pterminal_fds[2] = keyboard_timer_poll;
-
-  pway->events_fds = &pterminal_fds[0];
+  pway_set_app_fd(tty_fd);
 
   printf("running pterminal\n");
 
   while (terminal_window.is_running) {
-
-    pway_prepare_to_read_events();
-
-    if (poll(pterminal_fds, 3, -1) == -1) {
-      perror("Poll in fds, TTY or Wayland, Keyboard timer");
-    }
-
-    if( pterminal_fds[0].revents & POLLIN){
-      read_tty();
-    }
-
-
+    
     pway_handle_events();
 
-  
-    draw();
-    printf("draw\n");
+    if( pway_app_has_event() ){
+      read_tty();
+      draw();
+      printf("draw\n");
+    }
 
     clean_mouse_buttons();
+
   }
 }
--- pterminal.h
+++ pterminal.h
@@ -3,7 +3,6 @@
 
 #include <poll.h>
 #include <pway/pway.h>
-extern struct pollfd pterminal_fds[3];
 
 extern PWay* pway;
 void *run_pterminal(void *none);
